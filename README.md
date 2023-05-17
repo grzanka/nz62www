6287b969b5db132ab208656f9602dc14d67e9841
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-05-17 12:14:48 +0200

    new files

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-  * https://www.ifj.edu.pl/dept/no6/nz62/www/
-  * github pages https://grzanka.github.io/nz62www/
+# proton-irradiation-www
+Website for proton irradiation station at IFJ PAN:
+  - development version of the site: https://grzanka.github.io/nz62www/
+  - production version of the site: https://www.ifj.edu.pl/dept/no6/nz62/www/
 
+## Installation
+
+This repository uses git submodules, therefore simple git clone operation may not be enough to get all contents.
+Git submodules are used for Hugo static HTML generator theme.
 Clone the repo:
 
 ```bash
@@ -32,21 +38,31 @@ cd nz62www
 hugo
 ```
 
-Make a mirror of existing website:
+## Contents of the repository
 
-```bash
-lftp sftp://USERNAME:PASSWORD@web.ifj.edu.pl:PORT -e 'mirror --verbose --use-pget-n=8 -c /nz62 /sftp/grzanka/nz62www/backup/nz62_20221115'
-```
+The page is generated using [Hugo]https://gohugo.io). The repository contains:
+- `content/` - directory with the content of the page in the Markdown format
+- `config.toml` - configuration file for the Hugo generator
+- `layouts` - custom layouts adjustments for the theme
+- `themes` - directory with the customised Hugo docdock theme
+- `.env` - file with enviroment variables for connection details to deploy site to IFJ server (server, username, path)
+- `deploy.sh` - deploy script which builds the page and uploads it to IFJ server
 
-Deploy genereated website:
+Page layout is based on the [DocDock](https://docdock.vjeantet.fr) theme.
 
-```bash
-lftp sftp://USERNAME:PASSWORD@web.ifj.edu.pl:PORT -e 'mirror -R --verbose --use-pget-n=8 -c /sftp/grzanka/nz62www/public/ /nz62/'
-```
+Github Actions (customised in `.github/workflows/`) are used to run automatic tests after every commit. 
+These checks ensure that the page is generated correctly and that all links are valid.
 
-Fix permissions:
 
-```bash
-cd nz62
-chmod -R o+rx .
-```
+## Deployment
+
+LFTP deploy is handled by `deploy.sh` script. It assumes that necessary credentials are stored in the `.env` file of in the environment variables.
+You can use it to deploy site to the IFJ web server.
+
+## How to contribute
+
+If you want to contribute to the page, please follow these steps:
+1. Create new branch from the main `master` branch
+2. Make changes in the new branch
+3. Create pull request to the `master` branch
+4. Wait for the review and merge. Once the pull requests is merged, Github Actions will automatically deploy the page to https://grzanka.github.io/nz62www/
---
+++ deploy.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Check if python3 is installed
+if ! command -v python3 &> /dev/null
+then
+    echo "Python3 could not be found"
+    exit
+fi
+
+# Check if venv directory exists and create venv enviroment if it doesn't
+if [ ! -d "venv" ]
+then
+    echo "Creating virtual environment"
+    python3 -m venv venv
+fi
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Install and upgrade dependencies (mkdocs and others)
+pip install --requirement requirements.txt --upgrade
+
+# Remove old builds of mkdocs site
+rm -rf site
+
+# Build mkdocs site
+mkdocs build
+
+# Import all enviroment variables from .env file if it exists
+set -o allexport
+[[ -f .env ]] && source .env
+set +o allexport
+
+# Check if all enviroment variables needed to deploy site are set, if not, skip upload
+if [ -z "$LFTP_PASSWORD" ]
+then
+    echo "LFTP_PASSWORD environment variable is not set, skipping upload"
+elif [ -z "$LFTP_USER" ]
+then
+    echo "LFTP_USER environment variable is not set, skipping upload"
+elif [ -z "$LFTP_HOST" ]
+then
+    echo "LFTP_HOST environment variable is not set, skipping upload"
+elif [ -z "$LFTP_PORT" ]
+then
+    echo "LFTP_PORT environment variable is not set, skipping upload"
+elif [ -z "$LFTP_PATH" ]
+then
+    echo "LFTP_PATH environment variable is not set, skipping upload"
+else
+    echo "Uploading site to server from $PWD/site to $LFTP_PATH on $LFTP_HOST:$LFTP_PORT as $LFTP_USER"
+
+    # we start first by removing all previously uploaded files and directories from server
+    # as LFTP doesn't support recursive removal of files and directories, 
+    # we start first by list all files and directories on server
+    # and then remove them one by one
+    lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "cls $LFTP_PATH; quit" > server_contents.txt
+
+    # remove one-by-one all files and directories on server contained in local server_contents.txt file
+    while read -r line; do
+        if [[ $line == *"$LFTP_PATH"* ]]; then
+            echo "Removing $line from server"
+            lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "rm -rf $line; quit"
+        fi
+    done < server_contents.txt
+
+    # check if all the files were removed
+    echo "Checking if all files were removed"
+    lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "cls -al $LFTP_PATH; quit"
+
+    # upload all files and directories from local site/ directory to server
+    lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "mirror -R site/ $LFTP_PATH; quit"
+    
+    echo "Checking if all files were created"
+    lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "cls -al $LFTP_PATH; quit"
+
+    # at first glance it may look like that the mirror command should do the job, but unfortunately it doesn't
+    # all the uploaded files with get null permissions for "others" (0x640 for files and 0x750 for directories)
+    # to be able to serve the page we need to change the permissions of all files and directories to 0x644 and 0x755 respectively
+    # again, LFTP doesn't support recursive chmod, so we need to do it one by one, separately for files and directories
+    cd site
+    find . -type f -exec lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "chmod o+r $LFTP_PATH{}; quit" \;
+    find . -type d -exec lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "chmod o+rx $LFTP_PATH{}; quit" \;
+    cd ..
+
+    echo "Checking if all files were created with proper permissions"
+    lftp --env-password sftp://$LFTP_USER@$LFTP_HOST:$LFTP_PORT -e "cls -al $LFTP_PATH; quit"
+
+fi
+
+# Deactivate virtual environment
+deactivate
